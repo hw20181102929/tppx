d143c96395c0a15319a653562009b9cda6857644
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-12-04 12:01:11 +0800

    1
    1

--- tppx/main.c
+++ tppx/main.c
@@ -1,15 +1,33 @@
-//
-//  main.c
-//  tppx
-//
-//  Created by s20181102929 on 2019/12/4.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+bool IsTopSeq( LGraph Graph, Vertex Seq[] )
+{
+    int indegree[1000];
+    int p[1000];
+    int rear=0,front=0;
+    for(int i=0;i<Graph->Nv;i++)
+    {
+        indegree[i]=0;
+    }
+    struct AdjVNode * w;
+    for(int i=0;i<Graph->Nv;i++)
+    {
+        for (w=Graph->G[i].FirstEdge;w;w=w->Next)
+        {
+            indegree[w->AdjV]++;
+        }
+    }
+    for(int i=0;i<Graph->Nv;i++)
+    {
+        if(indegree[i]==0)
+        {
+            p[rear++]=i;
+        }
+    }
+    int c = 0;
+    while(rear!=front)
+    {
+        while(Seq[c]!=p[front])
+        {
+            
+        }
+    }
 }
